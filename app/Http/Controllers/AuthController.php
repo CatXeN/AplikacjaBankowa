50e6dac5e4ca1dab0eb2130ada8ba38887e8f67c
Kamil Kot <kamil.kot07@gmail.com>
2022-10-02 18:37:50 +0200

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,10 +13,30 @@ class AuthController extends Controller
         return view('login');
     }
 
-    // function checkLogin(Request $request)
-    // {
-    //     $this-> validate($request, [
-    //         'email' => 'required|email'
-    //     ])
-    // }
+    function checkLogin(Request $request)
+    {
+        $this-> validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|alphaNum|min:3'
+        ]);
+
+        $user_data = array(
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        );
+
+        if (Auth::attempt($user_data))
+        {
+            return redirect('');
+        }
+        else 
+        {
+            return back()->with('error', 'Błedne dane');
+        }
+    }
+
+    function successlogin() 
+    {
+
+    }
 }
